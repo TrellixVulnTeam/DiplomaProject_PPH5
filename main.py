c18b65e54dd10b486086ca60564b1a11b74b3d73
radia <radianoshop@gmail.com>
2021-04-25 16:11:11 +0200

    comment

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from kivy.lang import Builder
-from kivy.properties import StringProperty
+from kivy.properties import StringProperty, ObjectProperty
 from kivy.uix.screenmanager import Screen, ScreenManager
 from kivymd.app import MDApp
 from kivy.factory import Factory
@@ -13,24 +13,51 @@ from kivymd.uix.list import IRightBodyTouch, OneLineAvatarIconListItem, OneLineL
 from kivy.lang import Builder
 from kivy.properties import StringProperty
 
-g = Github("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 ")
+# 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
+g = Github("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")
+user = g.get_user() # this line is needed to specify user
+repos = user.get_repos() # array with repos
 
-user = g.get_user("radiano2")
+for x in repos:
+    if x.language != None and x.private == False:
+        print("{0} - repo name , {1} - language".format(x.name,x.language))
 
-repos = user.get_repos()
+
+
+class MainApp(MDApp):
+    screen = Screen()
+    scr_mngr = ObjectProperty(None)
+    tokenscreen = ObjectProperty(None)
+
+    def build(self):
+        self.theme_cls.primary_palette = 'Blue'
+        screen = Builder.load_file('layout.kv')
+        return screen
+
+    def set_screen(self, screen):
+        self.root.current = screen
+        print("switching screen to tokenscreen")
+
+    # def on_start(self):
+    #     tokenValue = self.scr_mngr.tokenscreen.tokenfield.text
+    #     g = Github(tokenValue)
+    #     user = g.get_user()
+    #     repos = user.get_repos()
 
 
 class TokenScreen(Screen):
+    # def getToken(self):
+    #     tokenValue = self.scr_mng.tokenID.tokenfield.text
+    #     g = Github(tokenValue)
+    #     user = g.get_user("radiano2")
+    #     repos = user.get_repos()
     pass
 
 
 class ListScreen(Screen):
+
     def on_pre_enter(self, *args):
-        # for x in repos:
-        #     if x.language != None:
-        #         self.ids.container.add_widget(
-        #             OneLineListItem(text="{0} - {1}".format(x.name, x.language))
-        #         )
+
         for x in repos:
             if x.language != None:
                 self.ids.container.add_widget(
@@ -46,20 +73,6 @@ sm = ScreenManager()
 sm.add_widget(TokenScreen(name='tokenscreen'))
 sm.add_widget(ListScreen(name='listscreen'))
 sm.add_widget(RepoScreen(name='reposcreen'))
-
-
-class MainApp(MDApp):
-    screen = Screen()
-
-    def build(self):
-        self.theme_cls.primary_palette = 'Blue'
-        screen = Builder.load_file('layout.kv')
-        return screen
-
-    def set_screen(self, screen):
-        self.root.current = screen
-        print("switching screen to tokenscreen")
-
 
 if __name__ == '__main__':
     MainApp().run()
