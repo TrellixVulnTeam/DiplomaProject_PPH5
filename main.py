209ba8ce1128c25daafdb63c64847fdb129fab9e
radia <radianoshop@gmail.com>
2021-05-30 23:27:07 +0200

    Added 2 new screens , added auth with the login and password , created second database and optimized code as a whole

--- main.py
+++ main.py
@@ -8,11 +8,9 @@ from kivymd.uix.list import TwoLineListItem
 from pymongo import MongoClient
 
 
-# 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
+# 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
 
 # Ddm8ti59QkyxPwqR
-
-
 
 
 class MainApp(MDApp):
@@ -32,12 +30,15 @@ class MainApp(MDApp):
             "mongodb+srv://radiano2:S4qa9ls4@pycluster.mph6u.mongodb.net/test?retryWrites=true&w=majority")
         db = client["test"]
         db.drop_collection("test")
+        db.drop_collection("loginCol")
+
 
 class TokenScreen(Screen):
     def on_pre_leave(self, *args):
-        tokenPassVar = self.ids.tokenFieldID.text
+        # tokenPassVar = self.ids.tokenFieldID.text
+        pass
 
-    def set_record(self):
+    def set_token_record(self):
         client = pymongo.MongoClient(
             "mongodb+srv://radiano2:S4qa9ls4@pycluster.mph6u.mongodb.net/test?retryWrites=true&w=majority")
         db = client["test"]
@@ -55,15 +56,19 @@ class ListScreen(Screen):
             "mongodb+srv://radiano2:S4qa9ls4@pycluster.mph6u.mongodb.net/test?retryWrites=true&w=majority")
         db = client["test"]
         collection = db["test"]
+        collection_login = db["loginCol"]
 
-        post = collection.find_one({"_id": 0})
-
-        return post["token"]
+        if collection.estimated_document_count() != 0:
+            post = collection.find_one({"_id": 0})
+            return Github(post["token"])
+        else:
+            post_login = collection_login.find_one({"_id": 0})
+            return Github(str(post_login["login"]), str(post_login["password"]))
 
     def on_pre_enter(self, *args):
         main_object = MainApp()
 
-        g = Github(self.load_token())
+        g = self.load_token()
 
         main_object.clearDB()
 
@@ -82,10 +87,29 @@ class RepoScreen(Screen):
     pass
 
 
+class LoginScreen(Screen):
+    def set_login_record(self):
+        client = pymongo.MongoClient(
+            "mongodb+srv://radiano2:S4qa9ls4@pycluster.mph6u.mongodb.net/test?retryWrites=true&w=majority")
+        db = client["test"]
+        collection = db["loginCol"]
+
+        post = {"_id": 0, "login": str(self.ids.loginFieldID.text)
+            , "password": str(self.ids.passwordFieldID.text)}
+
+        collection.insert_one(post)
+
+
+class OptionScreen(Screen):
+    pass
+
+
 sm = ScreenManager()
+sm.add_widget(OptionScreen(name='optionscreen'))
 sm.add_widget(TokenScreen(name='tokenscreen'))
 sm.add_widget(ListScreen(name='listscreen'))
 sm.add_widget(RepoScreen(name='reposcreen'))
+sm.add_widget(LoginScreen(name='loginscreen'))
 
 if __name__ == '__main__':
     MainApp().run()
